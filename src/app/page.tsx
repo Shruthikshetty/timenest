80f13d5110b333410952e49ec2d64cc8f8b0467b
shruthik shetty <shrutikshetty14@gmail.com>
2025-05-24 15:31:20 +0530

    add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "~/components/ui/Button";
+
 // thi is the first page of the app
 export default function Home() {
   return (
     <div className="flex-1">
-      <p className="text-red-300 text-6xl w-full mx-auto text-center">This is Next app</p>
+      <p className="text-red-300 text-6xl w-full mx-auto text-center">
+        This is Next app
+      </p>
+      <Button>Click me</Button>
     </div>
   );
 }
